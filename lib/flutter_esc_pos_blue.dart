db9f5ff3b8c08367b757523e7af2f72c47a5e64a
Onur Bulut <77685360+10urbulut@users.noreply.github.com>
2022-10-09 15:32:46 +0300

    initial

--- lib/flutter_esc_pos_blue.dart
+++ lib/flutter_esc_pos_blue.dart
@@ -5,7 +5,7 @@
  * Copyright (c) 2020. All rights reserved.
  * See LICENSE for distribution and usage details.
  */
-library esc_pos_bluetooth;
+library flutter_esc_pos_blue;
 
 export './src/enums.dart';
 export './src/printer_bluetooth_manager.dart';
